db3499d357e5959570421e48bddaafe73d1a7b01
douglasbogle <dbogle@bowdoin.edu>
2024-06-27 18:06:40 +0000

    fixingapi key issues

--- api_key.py
+++ api_key.py
@@ -4,18 +4,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+SPOTIFY_CLIENT_S = os.getenv('SPOTIFY_CLIENT_SECRET')
 
 def get_spotify_api_key():
     CLIENT_ID = SPOTIFY_CLIENT_ID
-    CLIENT_SECRET = SPOTIFY_CLIENT_SECRET
+    CLIENT_S = SPOTIFY_CLIENT_S
 
     AUTH_URL = "https://accounts.spotify.com/api/token"
 
     auth_response = requests.post(AUTH_URL, {
         "grant_type": "client_credentials", 
         "client_id": CLIENT_ID,
-        "client_secret" : CLIENT_SECRET,
+        "client_secret" : CLIENT_S,
     })
 
     auth_response_data = auth_response.json()
