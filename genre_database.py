7e6a4026eb0bc20464bfb48812e96ce2ae261689
douglasbogle <154115254+douglasbogle@users.noreply.github.com>
2024-07-02 20:07:15 -0400

    editing genre_database.py comments

--- genre_database.py
+++ genre_database.py
@@ -4,14 +4,14 @@ from spotify_genres import genres
 from api_key import *
 
 
-engine = db.create_engine('sqlite:///music_info.db')  # create database file
+engine = db.create_engine('sqlite:///music_info.db')  # create SQLite database engine
 
 
 def create_db(query_words):
     SPOTIFY_API_KEY = get_spotify_api_key()
     headers = {"Authorization": "Bearer {token}".format(token=SPOTIFY_API_KEY)}
 
-    for genre in genres:  # make a database table for every genre we have
+    for genre in genres: 
         final_query = f"{genre} {' '.join(query_words)}"
         params = {
             "q": final_query,
@@ -23,16 +23,16 @@ def create_db(query_words):
         data = response.json()
         songs_dict = {}
 
-        for i, item in enumerate(data['tracks']['items']):  # put all songs of one genre into dict
+        for i, item in enumerate(data['tracks']['items']):
             song_name = item['name']
             artist_name = item['artists'][0]['name']
             album_name = item['album']['name']
             song_link = item['external_urls']['spotify']
             songs_dict[i + 1] = {"song_name": song_name, "artist_name": artist_name, "album_name": album_name, "song_link": song_link}
 
-        dict_to_table(songs_dict, genre.replace(("-"), ""))  # create an sql table for this genre, remove - character as it causes errors
-
+        dict_to_table(songs_dict, genre.replace(("-"), ""))  # Remove - character from genre to avoid errors in SQLite
+        
 
 def dict_to_table(genre_dict, genre):
     df = pd.DataFrame.from_dict(genre_dict, orient='index')
-    df.to_sql(genre, con=engine, if_exists='replace')  # create a singular genre table in our already made database
+    df.to_sql(genre, con=engine, if_exists='replace') # Write DataFrame to SQLite table to represent a genre
